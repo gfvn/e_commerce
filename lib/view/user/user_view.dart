3bf3cf84d57b63af1f03203bfd35007147eb6e81
mostafa omar <dkshfri976@gmail.com>
2021-11-28 21:30:07 +0300

    initial commit

--- lib/view/user/user_view.dart
+++ lib/view/user/user_view.dart
@@ -1,3 +1,6 @@
+import 'package:e_commerce/shared/constants/constants.dart';
+import 'package:e_commerce/shared/style/dimensions.dart';
+import 'package:e_commerce/view/widgets/custom_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,18 +9,39 @@ class UserView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            CircleAvatar(
-              radius: 30.0,
-              child: Image.network('https://image.freepik.com/free-photo/happy-bearded-young-man-looks-with-joyful-expression-has-friendly-smile-wears-yellow-sweater-red-hat_295783-1388.jpg'),
-
-            )
-          ],
-        )
-      ],
+    return Padding(
+      padding: padding(context),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                  radius: 70.0,
+                  backgroundImage: NetworkImage(
+                      'https://image.freepik.com/free-photo/happy-bearded-young-man-looks-with-joyful-expression-has-friendly-smile-wears-yellow-sweater-red-hat_295783-1388.jpg')),
+              wSizedBox3,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width*0.4,
+                    child: const Text(
+                      'Mostafa Omar ',
+                      style: TextStyle(
+                        fontSize: 26,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  hSizedBox1,
+                  CustomText(text: 'dkshfri976@gmail.com', fontSize: 14.0),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
